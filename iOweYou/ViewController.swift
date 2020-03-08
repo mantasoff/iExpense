fe7d3e749466c7df3ec7f58ddf3e20cecd8a3a4e
mantasoff <mantas.paskevicius1996@gmail.com>
2020-03-08 21:05:54 +0200

    Initial Commit

---
+++ iOweYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOweYou
+//
+//  Created by Mantas Paškevičius on 2020-03-08.
+//  Copyright © 2020 Mantas Paškevičius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
